81569d2ab461b49527cb7945e0088242ff8688d0
Jeremias Tomé <jeremias.tome1@gmail.com>
2017-10-19 19:29:06 -0300

    Agregado script para unidades
    Permite seleccionar y mover una unidad con los botones del mouse

---
+++ Proyecto-RTS/Assets/Scripts/UnitController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UnitController : MonoBehaviour {
+
+    Rigidbody2D unitRB;
+    public float velocidad;
+    Vector2 target;
+    bool selected;
+
+    // Use this for initialization
+    void Start () {
+        target = transform.position;
+        unitRB = GetComponent<Rigidbody2D>();
+        selected = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+        if (Input.GetKeyDown(KeyCode.Mouse1) && selected)
+        {
+            target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+        }
+
+        if (Vector3.Distance(this.transform.position, target) > 0.1f)
+        {                
+            this.transform.position = Vector2.MoveTowards(this.transform.position, target, velocidad * Time.deltaTime);               
+        }
+        else
+        {
+            target = this.transform.position;
+                
+        }        
+    }
+
+    void OnMouseDown()
+    {
+        selected = !selected;      
+    }
+}
