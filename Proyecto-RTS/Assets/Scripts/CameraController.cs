7e43a099633bea6eafd2da8f2357ef634d993b5f
Jeremias Tomé <jeremias.tome1@gmail.com>
2017-10-19 17:32:21 -0300

    Agragados:
    Sprites: Agregados sprites para unidad caballero
    Animaciones: Creadas animaciones walking y stopped en todas las direcciones
    Camara: Creado script para mover la camara por el mapa

---
+++ Proyecto-RTS/Assets/Scripts/CameraController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+
+    Rigidbody2D cameraRB;
+    public float vel;
+
+    // Use this for initialization
+    void Start () {
+        cameraRB = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        cameraRB.velocity = new Vector2(moveHorizontal * vel, moveVertical * vel);
+    }
+}
