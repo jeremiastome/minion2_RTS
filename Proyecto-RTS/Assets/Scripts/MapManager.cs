eb553dec9566998888e380603fbaa29e07e5542f
Jeremias Tomé <jeremias.tome1@gmail.com>
2017-10-21 15:49:04 -0300

    MapManager
    Agregados
    + Script MapManger que genera la lista de porsiciones del mapa.
    + Script PositionScript para que la celda reaccione a colliciones y clicks del mouse.
    + Clase Position.

---
+++ Proyecto-RTS/Assets/Scripts/MapManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapManager : MonoBehaviour {
+
+    // Use this for initialization
+
+    List<PositionScript> positions;
+
+	void Start () {
+        positions = new List<PositionScript>();
+        GameObject grilla = GameObject.Find("Map").transform.Find("collision").gameObject;
+        Transform[] celdas = grilla.transform.GetComponentsInChildren<Transform>();
+
+        foreach (Transform c in celdas)
+        {
+            c.gameObject.AddComponent<PositionScript>();
+            PositionScript positionScript = c.gameObject.GetComponent<PositionScript>();
+            positionScript.SetPosition(new Position(c.position.x, c.position.y));
+            positions.Add(positionScript);
+        }
+
+        Debug.Log("Cant posiciones: " + positions.Count);
+       
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
